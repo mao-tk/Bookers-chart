3e6fffa96503cc32beaed36a297b592f6a1e217c
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-20 02:05:52 +0000

    [Add]User info完成/ Book index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
   def index
-    
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = current_user.books.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -8,5 +15,10 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
