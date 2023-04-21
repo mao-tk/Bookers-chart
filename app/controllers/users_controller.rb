79ed997929a94936a57168f65d28e4a127393abf
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-21 02:52:09 +0000

    [Add]flash/validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path(user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction)
   end
 end
