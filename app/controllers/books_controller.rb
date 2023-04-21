66c8d71e96006f5092e4831dc1d7c1ca2a3a237e
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-21 05:21:14 +0000

    [Finish]Bookers3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,20 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @book = Book.new
     @books = Book.all
   end
 
   def create
-    book = current_user.books.new(book_params)
-    book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(book.id)
+    @book = current_user.books.new(book_params)
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
   def show
@@ -21,10 +27,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -38,4 +47,13 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    user = book.user_id
+    unless user == current_user.id
+      redirect_to books_path
+    end
+  end
+
 end
